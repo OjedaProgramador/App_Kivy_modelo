1cbe1ba3a25900fef50464981affd3f2e6bb4724
Gabriel Ojeda <criatividadeojeda@hotmail.com>
2025-05-12 13:05:54 -0300

    certificado

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ from myfirebase import MyFireBase
 from telas import  *
 from botoes import *
 import requests
+import os
+import  certifi
 from bannervenda import BannerVenda
 import os
 from functools import partial
 from myfirebase import MyFireBase
 from bannervendedor import BannerVendedor
 from datetime import date
+
+os.environ['SSL_CERT_FILE'] = certifi.where() # o celular precisa dessa biblioteca para acessar a Internet 
 
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
